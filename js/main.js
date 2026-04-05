8587a6d75ba99d21471c7aa47ff2999d7a2be8ae
pragna21-dev <pragna21.patel@gmail.com>
2026-04-05 11:53:32 +0530

    Api call Post

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-var a;
+const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts?_limit=10';
+
+document.addEventListener('DOMContentLoaded', () => {
+	getPosts();
+});
+async function getPosts() {
+  try {
+    const res = await fetch(POSTS_URL);
+    const data = await res.json();
+    console.log(data);
+  } catch (error) {
+    console.log('Error:', error);
+  }
+}
